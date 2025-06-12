e9dd8d5c1d8d2d330235c74b839a0e7406362c4f
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2025-06-12 22:32:56 +0300

    fixing wrong typed url path name for pets app

--- workshop/petstagram/accounts/urls.py
+++ workshop/petstagram/accounts/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('profile/<int:pk>/', include([
-        path('', views.show_profile_details, name='profile_details'),
+        path('', views.show_profile_details, name='profile-details'),
         path('edit/', views.edit_profile, name='profile_edit'),
         path('delete/', views.delete_profile, name='profile_delete'),
     ]))
