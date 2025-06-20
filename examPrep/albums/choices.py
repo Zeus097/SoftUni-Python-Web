c80f0ffa13ccd1a2004484af9dd4c40a1c7a89c1
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2025-06-20 12:47:02 +0300

    Created models fro albums app and profiles app

---
+++ examPrep/albums/choices.py
@@ -0,0 +1,15 @@
+from django.db.models import TextChoices
+
+
+class GenreChoices(TextChoices):
+    POP_MUSIC = "Pop Music", "Pop Music"
+    JAZZ_MUSIC = "Jazz Music", "Jazz Music"
+    RNB_MUSIC = "R&B Music", "R&B Music"
+    ROCK_MUSIC = "Rock Music", "Rock Music"
+    COUNTRY_MUSIC = "Country Music", "Country Music"
+    DANCE_MUSIC = "Dance Music", "Dance Music"
+    HIP_HOP_MUSIC = "Hip Hop Music", "Hip Hop Music"
+    OTHER = "Other", "Other"
+
+
+
---
+++ examPrep/albums/models.py
@@ -0,0 +1,32 @@
+from django.core.validators import MinValueValidator
+from django.db import models
+from albums.choices import GenreChoices
+
+# Create your models here.
+
+
+class Album(models.Model):
+    album_name = models.CharField(
+        max_length=30,
+        unique=True,
+    )
+    artist_name = models.CharField(
+        max_length=30,
+    )
+    genre = models.CharField(
+        max_length=30,
+        choices=GenreChoices.choices
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+    )
+    image_url = models.URLField()
+    price = models.FloatField(
+        validators=[MinValueValidator(0)]
+    )
+    owner = models.ForeignKey(
+        'profiles.Profile',
+        on_delete=models.CASCADE,
+        related_name='albums'
+    )
